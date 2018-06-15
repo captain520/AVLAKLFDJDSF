921506969cd249b1de36e103cdb6ee82b494591a
captain520 <34092052+captain520@users.noreply.github.com>
2018-06-15 10:34:36 +0800

    111

---
+++ AVLAKLFDJDSF/CPTestObject.h
@@ -0,0 +1,13 @@
+//
+//  CPTestObject.h
+//  AVLAKLFDJDSF
+//
+//  Created by 王璋传 on 2018/6/15.
+//  Copyright © 2018年 王璋传. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CPTestObject : NSObject
+
+@end
---
+++ AVLAKLFDJDSF/CPTestObject.m
@@ -0,0 +1,13 @@
+//
+//  CPTestObject.m
+//  AVLAKLFDJDSF
+//
+//  Created by 王璋传 on 2018/6/15.
+//  Copyright © 2018年 王璋传. All rights reserved.
+//
+
+#import "CPTestObject.h"
+
+@implementation CPTestObject
+
+@end
